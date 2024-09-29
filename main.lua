645c487fcee1edfe88e6001046a1efc0506fcefc
Jason Weaver <indyplanets@gmail.com>
2024-09-29 07:13:54 -0500

    test square with fill color

--- main.lua
+++ main.lua
@@ -5,4 +5,6 @@ lick.reset = true
 function love.draw()
 	love.graphics.setColor(1, 0, 0)
 	love.graphics.circle("fill", 800 / 2, 600 / 2, 30)
+	love.graphics.setColor(0, 1, 0)
+	love.graphics.rectangle("fill", 100, 100, 300, 200)
 end
