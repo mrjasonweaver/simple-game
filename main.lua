59418ee2256310e9af9fc70e935ebd7c594a8b9b
Jason Weaver <indyplanets@gmail.com>
2024-09-29 19:39:29 -0500

    Adds comments: - load - update - draw *

--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
--- Hot reload
+-- Hot reload.
 local lick = require("lick")
 lick.reset = true
 
+-- Set up player position.
 local player = {
 	x = 20,
 	y = 20,
 }
 
-local SPEED = 300
+-- We want a speed of 200 pixels per move.
+local SPEED = 200
 
-function love.load() end
+function love.load()
+	-- load gameplay
+end
 
+-- Update game state with keyboard moves.
 function love.update(dt)
 	if love.keyboard.isDown("l") then
 		player.x = player.x + SPEED * dt
@@ -26,9 +31,10 @@ function love.update(dt)
 	end
 end
 
+-- Draw the simple graphics for gameplay.
 function love.draw()
-	love.graphics.setColor(1, 0, 0)
+	love.graphics.setColor(20 / 255, 200 / 255, 200 / 255)
 	love.graphics.rectangle("fill", player.x, player.y, 100, 20)
-	love.graphics.setColor(0, 1, 0)
+	love.graphics.setColor(100 / 255, 200 / 255, 20 / 255)
 	love.graphics.line(player.x + 20, player.y + 20, player.x + 100, player.y + 10)
 end
